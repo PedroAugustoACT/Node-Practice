3ae37c4493191269a42241ece5c758a8e2c55ee7
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-09-28 18:05:16 -0300

    tipos primitivos

---
+++ primitiveTypes/numbers.js
@@ -0,0 +1,30 @@
+//Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+let opeMath = primeiroNumero + segundoNumero;
+
+//console.log(numFloat);
+
+
+//Ponto Flutuante
+
+const numFloat = 3.3;
+const numFloat2 = .5;
+
+opeMath = meuNumero * (segundoNumero + primeiroNumero) / (numFloat - numFloat2);
+
+//console.log(opeMath);
+
+//NaN
+
+const string = 'Alura';
+
+for(let i = 0; i < meuNumero; i++){
+    console.log(string)
+}
+opeMath = string * primeiroNumero;
+console.log(opeMath);
---
+++ primitiveTypes/strings.js
@@ -0,0 +1,17 @@
+const text1 = 'Olá, Mundo!';
+const textNumbers = '18478237';
+
+//Não há diferença entre aspas simples e duplas
+
+console.log(text1);
+console.log(textNumbers);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
