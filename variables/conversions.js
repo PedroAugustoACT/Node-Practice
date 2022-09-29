68e86f82a7de0fca8f6bf09bfa7b91c0fff3e9f7
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-09-28 21:53:35 -0300

    variables

---
+++ variables/conversions.js
@@ -0,0 +1,15 @@
+//type
+//booleans
+
+//implicit conversion
+const number = 456;
+const numberString = '456';
+
+console.log(number === numberString);
+console.log(number == numberString);
+console.log(number + numberString);
+
+//explicit conversion
+console.log(number + Number(numberString));
+
+
---
+++ variables/truthy-false.js
@@ -0,0 +1,23 @@
+//boolean
+const userLog = true;
+const payment = false;
+
+//truthy or falsy
+
+// 0 -> false and 1 -> true
+
+console.log(0 == false);
+console.log('' == false);
+console.log(1 == true);
+
+let myVar; //undefined
+let varNull = null; //null -> empty or nothing
+
+console.log(typeof myVar);
+console.log(typeof varNull);
+
+let number = 3;
+let text = 'String';
+
+console.log(typeof number);
+console.log(typeof text);
