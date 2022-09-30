e5ad2b4aa6635515129b46e8d9869833773c08a3
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-09-30 16:39:50 -0300

    add comparators

---
+++ comparators/comparators.js
@@ -0,0 +1,12 @@
+// == (implicit comparison)
+
+const number = 5;
+const text = '5';
+
+console.log(number == text); //true
+console.log(number === text); //false
+
+// typeof
+
+console.log(typeof number);
+console.log(typeof text);
