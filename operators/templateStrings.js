4eeeb2c6bfe70f67e3225e119e9a87f5b1f6b901
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-10-03 18:11:38 -0300

    add operators

---
+++ operators/templateStrings.js
@@ -0,0 +1,7 @@
+const client = 'Pedro';
+const age = 2022-2002;
+const city = 'Maceió';
+//const presentation = 'My name is ' + client + ', i have ' + age + ' years old. I live in ' + city ;
+
+const presentation = `My name is ${client}, i have ${age} years old. I live in ${city}`;
+console.log(presentation);
---
+++ operators/ternary.js
@@ -0,0 +1,12 @@
+
+const minimalAge = 18;
+const clientAge = 1;
+
+// if(clientAge >= minimalAge){
+//     console.log('Liberado');
+// }
+// else{
+//     console.log('Proibido');
+// }
+
+console.log(clientAge >= minimalAge ? 'Liberado':'Proibido');
