e2f2257fcef2bf661ccbfab1cab053bbe0201023
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2023-01-09 16:08:16 -0300

    organiza arquivos e adiciona sort

---
+++ Assortment/Introduction/sort.js
@@ -0,0 +1,36 @@
+
+const numbers = [
+    {
+        title: 'Js',
+        value: 20
+    },
+    {
+        title: 'PHP',
+        value: 24
+    },
+    {
+        title: 'Ts',
+        value: 60
+    },
+    {
+        title: 'C',
+        value: 10
+    },
+    {
+        title: 'Python',
+        value: 50
+    },
+    {
+        title: 'Assembly',
+        value: 2
+    },
+    {
+        title: 'Ruby',
+        value: 15
+    },
+]
+
+
+console.log(numbers.sort((a,b) => a-b))
+
+//console.log(numbers.sort((a,b) => b-a))
